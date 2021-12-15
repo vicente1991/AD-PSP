43f2d0b47a9a46c38960415fa6eac60b0f224b0c
vicente <etenciv@gmail.com>
2021-12-15 20:49:17 +0100

    Final prueba

---
+++ E01-PracticaGuiadaVicenteRufo/E01-PracticaGuiadaVicenteRufo/src/test/java/com/example/E01PracticaGuiadaVicenteRufo/E01PracticaGuiadaVicenteRufoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E01PracticaGuiadaVicenteRufo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E01PracticaGuiadaVicenteRufoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
