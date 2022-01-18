282af5dda30e33c5a01d3b09a0229d82877d799d
vicente <etenciv@gmail.com>
2022-01-18 14:12:04 +0100

    Ejercicio Testing

---
+++ Testing/EjercicioMockito/src/test/java/com/salesianostriana/VentasTest.java
@@ -0,0 +1,30 @@
+package com.salesianostriana;
+
+
+import com.salesianostriana.dam.ejerciciotesting.repos.ClienteRepositorio;
+import com.salesianostriana.dam.ejerciciotesting.repos.ProductoRepositorio;
+import com.salesianostriana.dam.ejerciciotesting.repos.VentaRepositorio;
+import com.salesianostriana.dam.ejerciciotesting.services.VentaServicio;
+import lombok.experimental.ExtensionMethod;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+@ExtensionMethod(Mockito.class)
+public class VentasTest {
+
+    @InjectMocks
+    VentaServicio ventaServicio;
+
+    @Mock
+    VentaRepositorio ventaRepositorio;
+
+    @Mock
+    ClienteRepositorio clienteRepositorio;
+
+    @Mock
+    ProductoRepositorio productoRepositorio;
+
+
+
+}
