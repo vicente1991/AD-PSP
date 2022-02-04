72e8ef00173a61f774207c89d04119f05164d546
vicente <etenciv@gmail.com>
2022-02-04 14:25:37 +0100

    luismi loquillo

--- P01_Ficheros/src/main/java/org/example/UsandoFile.java
+++ P01_Ficheros/src/main/java/org/example/UsandoFile.java
@@ -11,8 +11,11 @@ public class UsandoFile {
         File f= new File("file.txt");
 
         try(PrintWriter pw= new PrintWriter(new FileWriter(f)
-                
+
         )){
+            pw.println("Ola k ase mercadona o k ase");
+            pw.flush();
+            System.out.println(pw);
 
         } catch (IOException e) {
             e.printStackTrace();
